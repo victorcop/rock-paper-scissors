359b0ab3d6f4e884d673788a438bb436fa70480f
Victor Velasquez <victor.1.velasquez-ext@jci.com>
2025-11-05 10:05:38 -0500

    Run Black formatter after isort"

--- src/rock_paper_scissors/__init__.py
+++ src/rock_paper_scissors/__init__.py
@@ -1,12 +1,15 @@
 """Rock Paper Scissors - A simple command-line game."""
 
-from rock_paper_scissors.rock_paper_scissors import (VALID_CHOICES,
-                                                     WIN_CONDITIONS, Choice,
-                                                     determine_winner,
-                                                     get_computer_choice,
-                                                     get_user_choice,
-                                                     play_game,
-                                                     play_multiple_rounds)
+from rock_paper_scissors.rock_paper_scissors import (
+    VALID_CHOICES,
+    WIN_CONDITIONS,
+    Choice,
+    determine_winner,
+    get_computer_choice,
+    get_user_choice,
+    play_game,
+    play_multiple_rounds,
+)
 
 __version__ = "1.0.0"
 __all__ = [
